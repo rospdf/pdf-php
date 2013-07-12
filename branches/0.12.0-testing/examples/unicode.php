7b704d06e2d929c61248da087b8b009063b61e25
ole1986 <ole1986@fcaf7de4-1243-4137-b908-a7e1c955c6d2>
2013-07-12 15:33:03 +0000

    ole1986: test environment for new TTF techniques ole1986: also test to get rid of checkTextDirective as it is to much complicated I guess

---
+++ branches/0.12.0-testing/examples/unicode.php
@@ -0,0 +1,54 @@
+<?php
+error_reporting(E_ALL);
+set_time_limit(1800);
+set_include_path('../src/' . PATH_SEPARATOR . get_include_path());
+
+include 'Cezpdf.php';
+
+class Creport extends Cezpdf{
+	function Creport($p,$o){
+  		$this->__construct($p, $o,'none',array());
+  		$this->isUnicode = true;
+  		// always embed the font for the time being
+  		//$this->embedFont = false;
+	}
+}
+$pdf = new Creport('a4','portrait');
+$pdf->ezSetMargins(20,20,20,20);
+//$pdf->rtl = true; // all text output to "right to left"
+//$pdf->setPreferences('Direction','R2L'); // optional: set the preferences to "Right To Left"
+
+$f = (isset($_GET['font']))?$_GET['font']:'FreeSerif';
+
+$mainFont = '../src/fonts/'.$f;
+// select a font and use font subsetting
+$pdf->selectFont($mainFont, '', 1, true);
+
+
+//$pdf->ezText("Some European special chars:");
+$pdf->ezText("ABC ÄäÀàÁáÂâÃãÅåǍǎĄą<u> ÇçĐď </u>ÈèÉéÊêËëĚěĘę");
+//$pdf->ezText("Cyrillic:");
+//$pdf->ezText("<u>КкЛлМмНнОоПпРрСсТтУу</u>");
+//$pdf->ezText("Arabic:");
+//$pdf->ezText("لبسبيلتتاف لالبالفقث بببب");
+//$pdf->ezText("Hebrew:");
+//$pdf->ezText("אבגדהוזחטיכלמנסעפצקרשת");
+//$pdf->ezText("Chinese:");
+//$pdf->ezText("汉语/漢語 <- Some fonts might not contain these glyphs. Tested with Arial Unicode");
+
+//$pdf->isUnicode = false;
+//$pdf->selectFont('../src/fonts/Courier');
+//$pdf->ezText("\nThis text is using Courier in a non-unicode standard");
+
+// reusing the mainFont does not require to enable unicode with $this->isUnicode
+
+if (isset($_GET['d']) && $_GET['d']){
+  $pdfcode = $pdf->ezOutput(1);
+  $pdfcode = str_replace("\n","\n<br>",htmlspecialchars($pdfcode));
+  echo '<html><body>';
+  echo trim($pdfcode);
+  echo '</body></html>';
+} else {
+  $pdf->ezStream();
+}
+?>
