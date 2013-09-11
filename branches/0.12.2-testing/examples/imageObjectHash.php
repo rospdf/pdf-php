c67976188b38b1f8af1635140b2181f56763770c
ole1986 <ole1986@fcaf7de4-1243-4137-b908-a7e1c955c6d2>
2013-09-11 08:43:16 +0000

    changed image only in examples/imageObjectHash.php

--- branches/0.12.2-testing/examples/imageObjectHash.php
+++ branches/0.12.2-testing/examples/imageObjectHash.php
@@ -28,13 +28,13 @@ $height = $pdf->getFontHeight($size);
 $pdf->openHere('Fit');
 
 $pdf->ezText("Since version 011 object hash is enabled to reduce the pdf size when redundant images are used");
-$pdf->ezText("This image below has a size of <b>".filesize('images/test_alpha2.png').' bytes</b>');
+$pdf->ezText("This image below has a size of <b>".filesize('../ros.jpg').' bytes</b>');
 $pdf->ezText("So the object is being hashed and reused 3 times in this examples");
 $pdf->ezText("The XObject always refers to the same object number.");
 $pdf->ezText("Put <b>'?nohash'</b> to disable object hashing\n\n");
-$pdf->ezImage('images/test_alpha2.png',0,0,'none','left');
-$pdf->ezImage('images/test_alpha2.png',0,0,'none','center');
-$pdf->ezImage('images/test_alpha2.png',0,0,'none','right');
+$pdf->ezImage('../ros.jpg',0,0,'none','left');
+$pdf->ezImage('../ros.jpg',0,0,'none','center');
+$pdf->ezImage('../ros.jpg',0,0,'none','right');
 
 if (isset($_GET['d']) && $_GET['d']){
   $pdfcode = $pdf->ezOutput(1);
