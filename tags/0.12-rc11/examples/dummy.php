a73d129237d4d06ec939844f8c8213c07cf9304e
ole1986 <ole1986@fcaf7de4-1243-4137-b908-a7e1c955c6d2>
2013-12-04 14:22:26 +0000

    -- version 0.12-rc11 --- - applied path #20 ezTable advanced grid lines (see examples/tables.php) - fixed bug #75 Alignment fixes for tables and images - applied patch #19 Table heading alignment - fixed an old bug related to vertical lines in ezTable when page break occurs

---
+++ tags/0.12-rc11/examples/dummy.php
@@ -0,0 +1,18 @@
+<?php
+$ext = '../extensions/CezDummy.php';
+if(!file_exists($ext)){
+	die('This example requires the CezDummy.php extension');
+}
+
+include $ext;
+$pdf = new CezDummy("a4");
+
+$pdf->selectFont('Helvetica');
+
+$pdf->ezText("Check the CezDummy.php extension to find the data being displayed\n");
+$pdf->ezText("<b>IMPORTANT:\nIn version >= 0.12.0 it is required to allow custom tags (by using \$pdf->allowedTags) before using it</b>\n");
+$pdf->ezText("<C:dummy:0>");
+$pdf->ezText("<C:dummy:1>");
+
+$pdf->ezStream();
+?>
