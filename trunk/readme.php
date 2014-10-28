5519cd6c27c4a34fcb19aa5ea49ccbc002049672
ole1986 <ole1986@fcaf7de4-1243-4137-b908-a7e1c955c6d2>
2014-10-28 10:50:30 +0000

    version increased

--- trunk/readme.php
+++ trunk/readme.php
@@ -74,7 +74,7 @@ function dots($info){
 // this code has been modified to use ezpdf.
 
 $project_url = "http://pdf-php.sf.net";
-$project_version = "0.12-rc17";
+$project_version = "0.12-rc18";
 
 $pdf = new Creport('a4','portrait', 'none', null);
 // to test on windows xampp
