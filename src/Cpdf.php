c35e787210f5402d2a8a364756d65d10a1e5182d
Aron Novak <aron.novak@agenceinovae.com>
2016-12-19 17:03:59 +0100

    Fix potential PHP notice in Cpdf->uniord()

--- src/Cpdf.php
+++ src/Cpdf.php
@@ -3056,10 +3056,10 @@
     {
         // important condition to allow char "0" (zero) being converted to decimal
         if(strlen($c) <= 0) return false;
-        $ord0 = ord($c{0}); if ($ord0>=0   && $ord0<=127) return $ord0;
-        $ord1 = ord($c{1}); if ($ord0>=192 && $ord0<=223) return ($ord0-192)*64 + ($ord1-128);
-        $ord2 = ord($c{2}); if ($ord0>=224 && $ord0<=239) return ($ord0-224)*4096 + ($ord1-128)*64 + ($ord2-128);
-        $ord3 = ord($c{3}); if ($ord0>=240 && $ord0<=247) return ($ord0-240)*262144 + ($ord1-128)*4096 + ($ord2-128)*64 + ($ord3-128);
+        $ord0 = isset($c{0}) ? ord($c{0}) : -1; if ($ord0>=0   && $ord0<=127) return $ord0;
+        $ord1 = isset($c{1}) ? ord($c{1}) : -1; if ($ord0>=192 && $ord0<=223) return ($ord0-192)*64 + ($ord1-128);
+        $ord2 = isset($c{2}) ? ord($c{2}) : -1; if ($ord0>=224 && $ord0<=239) return ($ord0-224)*4096 + ($ord1-128)*64 + ($ord2-128);
+        $ord3 = isset($c{3}) ? ord($c{3}) : -1; if ($ord0>=240 && $ord0<=247) return ($ord0-240)*262144 + ($ord1-128)*4096 + ($ord2-128)*64 + ($ord3-128);
         return false;
     }
     
