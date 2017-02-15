47805adf9a62d8f524b0b4d4722585840acfc8b9
Lucas S. Vieira <lucassouzavieiraengcomp@gmail.com>
2017-02-15 18:08:31 -0300

    PSR-2

--- examples/annotations.php
+++ examples/annotations.php
@@ -1,33 +1,36 @@
 <?php
+
 error_reporting(E_ALL);
 set_time_limit(1800);
-set_include_path('../src/' . PATH_SEPARATOR . get_include_path());
+set_include_path('../src/'.PATH_SEPARATOR.get_include_path());
 
 include 'Cezpdf.php';
 
-class Creport extends Cezpdf{
-	function Creport($p,$o){
-  		$this->__construct($p, $o,'none',array());
-	}
+class Creport extends Cezpdf
+{
+    public function Creport($p, $o)
+    {
+        $this->__construct($p, $o, 'none', array());
+    }
 }
-$pdf = new Creport('a4','portrait');
+$pdf = new Creport('a4', 'portrait');
 
 // to test on windows xampp
-if(strpos(PHP_OS, 'WIN') !== false){
+if (strpos(PHP_OS, 'WIN') !== false) {
     $pdf->tempPath = 'C:/temp';
 }
 
 // IMPORTANT: In version >= 0.12.0 it is required to allow custom tags (by using $pdf->allowedTags) before using it
-$pdf->allowedTags .= "|comment:.*?";
+$pdf->allowedTags .= '|comment:.*?';
 
-$pdf->ezSetMargins(20,20,20,20);
+$pdf->ezSetMargins(20, 20, 20, 20);
 
 $pdf->selectFont('Helvetica');
 $pdf->ezText("Some annotations are only shown in Adobe Reader. Chrome Viewer for instance does not show the icons\n");
 // text annotation (also know as comments)
 $pdf->ezText("<b>The 'Text' annotation:</b>");
 $pdf->ezText("This Example shows how easy it is to put comments like this (<C:comment:Hello World comment text>) in between of some text lines.\nHere is another one <C:comment:Isn't it cool?> in between\n");
-$pdf->addComment("Fixed position", "This comment is set to a fixed position by\nusing the addComment method explicitly", 500, $pdf->y + 20);
+$pdf->addComment('Fixed position', "This comment is set to a fixed position by\nusing the addComment method explicitly", 500, $pdf->y + 20);
 // external links
 $pdf->ezText("<strong>The 'External Link' annotation:</strong>");
 $pdf->ezText("This is an <c:alink:http://pdf-php.sf.net>external</c:alink> link.\n");
@@ -36,11 +39,10 @@ $pdf->addDestination('test001', 'FitH', $pdf->y);
 $pdf->ezText("<strong>The 'Internal Link' annotation:</strong>");
 $pdf->ezText("Followed by an <c:ilink:test001>internal</c:ilink> link which requires to set a destination first. Use \$pdf->addDestination() before adding internal links.\n");
 
-$pdf->ezText("More annotations soon...");
+$pdf->ezText('More annotations soon...');
 
-if (isset($_GET['d']) && $_GET['d']){
-  echo $pdf->ezOutput(TRUE);
+if (isset($_GET['d']) && $_GET['d']) {
+    echo $pdf->ezOutput(true);
 } else {
-  $pdf->ezStream(array('compress'=>0));
+    $pdf->ezStream(array('compress' => 0));
 }
-?>
--- examples/dummy.php
+++ examples/dummy.php
@@ -1,14 +1,15 @@
 <?php
+
 $ext = '../extensions/CezDummy.php';
-if(!file_exists($ext)){
-	die('This example requires the CezDummy.php extension');
+if (!file_exists($ext)) {
+    die('This example requires the CezDummy.php extension');
 }
 
 include $ext;
-$pdf = new CezDummy("a4");
+$pdf = new CezDummy('a4');
 
 // to test on windows xampp
-if(strpos(PHP_OS, 'WIN') !== false){
+if (strpos(PHP_OS, 'WIN') !== false) {
     $pdf->tempPath = 'C:/temp';
 }
 
@@ -16,8 +17,7 @@ $pdf->selectFont('Helvetica');
 
 $pdf->ezText("Check the CezDummy.php extension to find the data being displayed\n");
 $pdf->ezText("<b>IMPORTANT:\nIn version >= 0.12.0 it is required to allow custom tags (by using \$pdf->allowedTags) before using it</b>\n");
-$pdf->ezText("<C:dummy:0>");
-$pdf->ezText("<C:dummy:1>");
+$pdf->ezText('<C:dummy:0>');
+$pdf->ezText('<C:dummy:1>');
 
 $pdf->ezStream();
-?>
