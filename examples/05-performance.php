03499aade83cc1823f9fbcc106351fb10ab01bc5
ole <ole.k@web.de>
2017-04-21 12:55:04 +0200

    renamed and fixed performance example page

--- examples/05-performance.php
+++ examples/05-performance.php
@@ -15,16 +15,14 @@ $pdf = new CpdfExtension(Cpdf::$Layout['A4']);
 // Show page numbers - make use of 'repeat' function while paging
 $pagerText = $pdf->NewText(array(20,10,575,20));
 // allow custom callback 'pager' which is located in CpdfExtension class
-$pagerText->AllowedTags .= '|pager';
 $pagerText->SetPageMode(CpdfContent::PMODE_REPEAT); // repeat object from being parsed
 $pagerText->SetFont('Helvetica', 6, 'b');
-$pagerText->AddText("<c:pager>###</c:pager>", null, 'center'); // use a custom callback to display the page number
+$pagerText->AddText("<C:pager>", null, 'center'); // use a custom callback to display the page number
 
 // TODO: The paging cause wrong Y position, need to get fixed
 $textObject = $pdf->NewText();
 //$textObject->SetFont('Helvetica', 10);
-$textObject->BreakPage = true; // allow/disallow page breaks - default: true
-$textObject->BreakColumn = false; // allow/disallow column breaks if it fits to the page - default: false
+
 for ($i = 1; $i <= 2000; $i++) {
     $textObject->AddText("Lorem ipsum dol Lorem ipsum dol Lorem ipsum dol Lorem ipsum dol Lorem ipsum dol $i");
 }
