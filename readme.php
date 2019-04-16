7a934225f691101b6398ef0e5acf06127a943dfb
ole1986 <ole.k@web.de>
2019-04-16 21:54:36 +0200

    Version 0.12.51 (PSR only)

--- readme.php
+++ readme.php
@@ -72,7 +72,7 @@ class Creport extends Cezpdf
 // this code has been modified to use ezpdf.
 
 $project_url = "https://github.com/rospdf/";
-$project_version = "0.12.50";
+$project_version = "0.12.51";
 
 $pdf = new Creport('a4', 'portrait', 'none', null);
 
