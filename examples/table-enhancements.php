ac9a05e19bd13e583e0d0818f0bd707fafc13011
ole1986 <ole.k@web.de>
2019-07-27 16:33:42 +0200

    Updated table enhancements example

--- examples/table-enhancements.php
+++ examples/table-enhancements.php
@@ -9,25 +9,32 @@ $pdf = new CezPDF('a4');
 $pdf->selectFont('Helvetica');
 
 // some general data used for table output
-$data = array(
-array('num' => 1, 'name' => 'gandalf', 'type' => 'wizard', 'typeFill' => array(0, 1, 0)), array('num' => 2, 'name' => 'bilbo', 'type' => 'hobbit', 'typeFill' => array(0.588235294118, 0.407843137255, 0)), array('num' => 3, 'name' => 'frodo', 'type' => 'hobbit', 'typeFill' => array(0.588235294118, 0.407843137255, 0)), array('num' => 4, 'name' => 'saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman saruman', 'type' => 'bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude bad dude', 'typeFill' => array(1, 0, 0), 'typeColor' => array(1, 1, 1)), array('num' => 5, 'name' => 'sauron', 'type' => 'really bad dude', 'typeFill' => array(0, 0, 0), 'typeColor' => array(1, 1, 1)),
-);
+$data = [
+    ['num' => 1, 'numColor' => [1,1,1], 'name' => 'gandalf', 'type' => 'wizard', 'typeFill' => [0, 1, 0], 'typeColor' => [1,0,0]],
+    ['num' => 2, 'numColor' => [1,1,1], 'name' => 'bilbo', 'type' => 'hobbit'],
+    ['num' => 3, 'numColor' => [1,1,1], 'name' => 'frodo', 'type' => 'hobbit'],
+    ['num' => 4, 'numColor' => [1,1,1], 'name' => str_repeat('saruman ', 300), 'type' => str_repeat('bad dude', 300), 'typeFill' => [197/255, 213/255, 203/255], 'typeColor' => [159/255, 168/255, 163/255]],
+    ['num' => 5, 'numColor' => [1,1,1], 'name' => 'sauron', 'type' => 'really bad dude', 'typeFill' => [0, 0, 0], 'typeColor' => [1, 1, 1]],
+];
 
 $cols = array('num' => 'No', 'type' => 'Type', 'name' => '<i>Alias</i>');
 
-$conf = array(
-'evenColumns' => 2,
-'evenColumnsMin' => 40,
-'maxWidth' => 350,
-'shadeHeadingCol' => array(0.6, 0.6, 0.5),
-'shaded' => 1,
-'shadeCol' => array(0.95, 0.95, 0.95),
-'shadeCol2' => array(0.85, 0.85, 0.85),
-'xPos' => 'right',
-'xOrientation' => 'left',
-'gridlines' => 31,
-'cols' => array('num' => array('bgcolor' => array(1, 1, 0))),
-);
+$conf = [
+    'evenColumns' => 2,
+    'evenColumnsMin' => 40,
+    'maxWidth' => 350,
+    'shadeHeadingCol' => [23/255, 62/255, 67/255],
+    'textCol' => [1,1,1],
+    'shaded' => 2,
+    'shadeCol' => [0.95, 0.95, 0.95],
+    'shadeCol2' => [0.75, 0.75, 0.75],
+    'xPos' => 'right',
+    'xOrientation' => 'left',
+    'gridlines' => 31,
+    'cols' => [
+        'num' => ['bgcolor' => [90/255, 118/255, 112/255]]
+    ] // global background color for 'num' column
+];
 
 $pdf->ezTable($data, $cols, '', $conf);
 
