d9d335d54f1cd87ec4d23cc500122371a70392f1
ole1986 <ole.k@web.de>
2019-08-02 16:59:33 +0200

    Possible fix for #129

--- src/Cezpdf.php
+++ src/Cezpdf.php
@@ -1742,9 +1742,11 @@ class Cezpdf extends Cpdf
 
                                         // apply the color to the text
                                         if (is_array($textColor)) {
-                                            $line = $this->addText($pos[$colName], $this->y, $options['fontSize'], "<c:color:".$textColor[0].",".$textColor[1].",".$textColor[2].">".$line."</c:color>", $maxWidth[$colName], $just);
+                                            $this->setColor($textColor[0],$textColor[1],$textColor[2]);
+                                            $line = $this->addText($pos[$colName], $this->y, $options['fontSize'], $line, $maxWidth[$colName], $just);
                                         } else {
-                                            $line = $this->addText($pos[$colName], $this->y, $options['fontSize'], "<c:color:".$options['textCol'][0].",".$options['textCol'][1].",".$options['textCol'][2].">".$line."</c:color>", $maxWidth[$colName], $just);
+                                            $this->setColor($options['textCol'][0], $options['textCol'][1], $options['textCol'][2]);
+                                            $line = $this->addText($pos[$colName], $this->y, $options['fontSize'], $line, $maxWidth[$colName], $just);
                                         }
                                     }
                                 }
